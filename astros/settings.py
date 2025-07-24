572aded7b7c7d863c1d8096d164d8e94af932205
AdibBinKadir <adibbinkadir@gmail.com>
2025-07-24 09:07:48 -0500

    Removed hardcoded DJANGO_SECRET_KEY

--- astros/settings.py
+++ astros/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v-qyyuyw*dy^=#0t4(5c1vdj3dw=^0zz*!@xe^3ty!b68y#8qy'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
