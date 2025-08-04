b4cf78c1af199a4f877f44af2427f45ff4c8547e
AdibBinKadir <adibbinkadir@gmail.com>
2025-08-04 08:44:05 -0500

    Removed staticfiles

--- staticfiles/js/forgotpassword.js
+++
@@ -1,3 +0,0 @@
-        document.getElementById('reroute').addEventListener('click', function() {
-            window.location.href = '/';
-        });
--- staticfiles/js/products_dne.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    setTimeout(load, 1000);
-};
-function load(){
-    document.getElementById("loading").style.display = "none";
-    document.getElementById("main").style.display = "block";
-}
